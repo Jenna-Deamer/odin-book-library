68be8d444980b30881108eb235107d2da6760d90
Jenna Deamer <jennadeamer@gmail.com>
2025-03-10 16:11:16 -0400

    Add divs in DisplayAllBooks() for styles

--- scripts.js
+++ scripts.js
@@ -2,39 +2,43 @@ const BOOKCONTAINER = document.querySelector("#book-container");
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = read;
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = read;
 }
 
 function addBookToLibrary(title, author, pages, isRead) {
-    // create book
-    let newBook = new Book(title, author, pages, isRead);
-    // store in array
-    myLibrary.push(newBook);
-    console.log(myLibrary);
-    return myLibrary;
+  // create book
+  let newBook = new Book(title, author, pages, isRead);
+  // store in array
+  myLibrary.push(newBook);
+  console.log(myLibrary);
+  return myLibrary;
 }
 
 function displayAllBooks() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        // create a div & give it a class for every item in array
-        const bookCard = document.createElement("div");
-        bookCard.classList.add(".book-card");
+  for (let i = 0; i < myLibrary.length; i++) {
+    // create a div & give it a class for every item in array
+    const bookCard = document.createElement("div");
+    bookCard.classList.add(".book-card");
 
-        // define book-card content
-        const content = `
-       <h3>${myLibrary[i].title}</h3>
-       <p>By: ${myLibrary[i].author}</p>
-       <p>Pages: ${myLibrary[i].pages}</p>
-       <p>Read: ${myLibrary[i].isRead}</p>
+    // define book-card content
+    const content = `
+            <div class="book-card">
+        <div class="book-card-body">
+            <h3>${myLibrary[i].title}</h3>
+            <p>By: ${myLibrary[i].author}</p>
+            <p>Pages: ${myLibrary[i].pages}</p>
+            <p>Read: ${myLibrary[i].isRead}</p>
+        </div>
+    </div>   
       `;
 
-        // add bookCard to container
-        BOOKCONTAINER.innerHTML += content;
-    }
+    // add bookCard to container
+    BOOKCONTAINER.innerHTML += content;
+  }
 }
 
 displayAllBooks();
