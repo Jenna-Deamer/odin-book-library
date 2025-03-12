50f43116aca512ea3b97fbb1e6d33ffbff748b31
Jenna <jennadeamer@gmail.com>
2025-03-11 20:36:54 -0400

    Add form input values to script

--- scripts.js
+++ scripts.js
@@ -1,36 +1,39 @@
 const bookContainer = document.querySelector("#book-container");
-const dialog = document.querySelector("#add-book-modal")
+const dialog = document.querySelector("#add-book-modal");
 const showButton = document.querySelector("dialog + button");
-const  closeButton = document.querySelector("dialog button");
-const form = document.querySelector('form')
-
+const closeButton = document.querySelector("dialog button");
+const form = document.querySelector("form");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const isRead = document.querySelector("#is_read");
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
-  this.id = crypto.randomUUID();
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = read;
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = read;
 }
 
 function addBookToLibrary(title, author, pages, isRead) {
-  // create book
-  let newBook = new Book(title, author, pages, isRead);
-  // store in array
-  myLibrary.push(newBook);
-  console.log(myLibrary);
-  return myLibrary;
+    // create book
+    let newBook = new Book(title, author, pages, isRead);
+    // store in array
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+    return myLibrary;
 }
 
 function displayAllBooks() {
-  for (let i = 0; i < myLibrary.length; i++) {
-    // create a div & give it a class for every item in array
-    const bookCard = document.createElement("div");
-    bookCard.classList.add(".book-card");
+    for (let i = 0; i < myLibrary.length; i++) {
+        // create a div & give it a class for every item in array
+        const bookCard = document.createElement("div");
+        bookCard.classList.add(".book-card");
 
-    // define book-card content
-    const content = `
+        // define book-card content
+        const content = `
             <div class="book-card">
         <div class="book-card-body">
             <h3>${myLibrary[i].title}</h3>
@@ -41,22 +44,22 @@ function displayAllBooks() {
     </div>   
       `;
 
-    // add bookCard to container
-    bookContainer.innerHTML += content;
-  }
+        // add bookCard to container
+        bookContainer.innerHTML += content;
+    }
 }
 
 // New Book Modal
-showButton.addEventListener("click", () =>{
+showButton.addEventListener("click", () => {
     dialog.showModal();
 });
 
-closeButton.addEventListener("click", () =>{
+closeButton.addEventListener("click", () => {
     dialog.close();
 });
 
-form.addEventListener("submit", function(event){
+form.addEventListener("submit", function (event) {
     event.preventDefault();
 
-
+    console.log(title.value);
 });
